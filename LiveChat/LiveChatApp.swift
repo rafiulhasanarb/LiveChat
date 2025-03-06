9b2b4584ed7722de148a21b4d59173e19c602da7
rafiulhasanarb <rafiulhasanarb@gmail.com>
2025-03-07 01:27:09 +0600

    Initial Commit

---
+++ LiveChat/LiveChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveChatApp.swift
+//  LiveChat
+//
+//  Created by Rafiul Hasan on 3/7/25.
+//
+
+import SwiftUI
+
+@main
+struct LiveChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
